b9d4516921ed3068deb4992d32c7d72b9923054f
TEST DBT <ameenas.in@mouritech.com>
2025-03-10 10:44:41 +0000

    commit

---
+++ models/MY_FOLDER/practice.sql
@@ -0,0 +1,43 @@
+
+{##
+{% set my_name ='Shyam Sundar' %}
+{% set my_age =24 %}
+
+{{my_name}} age is {{my_age}}
+
+
+{% set animals=['Elephant','Lion','deer','cow']%}
+
+{% for i in animals%}
+    {% if{{i}} == 'Lion' %}
+        wild
+    {% else %}
+        non-wild
+    {% end if %}
+{% endfor %}
+
+
+{% set animals = ['Elephant', 'Lion', 'deer', 'cow'] %}
+
+{% for i in animals %}
+    {% if i == 'Lion' %}
+        wild
+    {% else %}
+        non-wild
+    {% endif %}
+{% endfor %}
+
+
+{%- set animals = ['Elephant', 'Lion', 'deer', 'cow'] -%}
+
+{%- for i in animals -%}
+    {%- if i == 'Lion' -%}
+        {%- set type= 'wild' -%}
+    {{type}}
+    {%- else -%}
+        {%- set type= 'non_wild' -%}
+    {{type}}
+    {% endif %}
+{% endfor %}
+
+#}
